0aad77776074b03efc64fe4b579e881f442b1815
Kot <1192090+koteq@users.noreply.github.com>
2022-12-05 14:42:10 +0400

    Solve day 5

---
+++ 2022/05/a.mjs
@@ -0,0 +1,45 @@
+import fs from "fs";
+
+class State {
+  static parse(str) {
+    const state = [];
+    str
+      .split("\n")
+      .slice(0, -1)
+      .reverse()
+      .forEach((row) => {
+        Array.from(row.matchAll(/.(.). ?/g)).forEach(([, crate], pos) => {
+          if (!state[pos]) {
+            state[pos] = [];
+          }
+          if (crate !== " ") {
+            state[pos].push(crate);
+          }
+        });
+      });
+    return new State(state);
+  }
+
+  constructor(state) {
+    this.state = state;
+  }
+
+  move(move, from, to) {
+    for (let i = 0; i < move; i++) {
+      this.state[to - 1].push(this.state[from - 1].pop());
+    }
+  }
+}
+
+const [stateString, movesString] = fs
+  .readFileSync("in")
+  .toString()
+  .trimEnd()
+  .split("\n\n");
+const state = State.parse(stateString);
+movesString.split("\n").forEach((line) => {
+  const [, move, from, to] = line.match(/move (\d+) from (\d) to (\d)/);
+  state.move(Number(move), Number(from), Number(to));
+});
+
+console.log(state.state.map((col) => col.pop()).join(""));
