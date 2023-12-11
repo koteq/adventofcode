7059189f7813cc6a07f391c4237b2bcc3a819408
Kot <1192090+koteq@users.noreply.github.com>
2023-12-11 14:56:52 +0400

    Update boilerplate

--- boilerplate/b.mjs
+++ boilerplate/b.mjs
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 
-function main(input_file = "s1") {
-  fs.readFileSync(input_file).toString().trim().split("\n");
+function main(inputFile = "s1") {
+  fs.readFileSync(inputFile).toString().trim().split("\n");
 }
 
 main();
